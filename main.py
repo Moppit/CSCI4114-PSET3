85d524fc75933fc1ed67dae1addb396a26068d93
Moppit <michelletranusa@gmail.com>
2020-09-15 22:44:42 -0600

    Finished backtracking algo -- it is very slow, and can get stuck on a few 9x9 -- fix if given time

--- main.py
+++ main.py
@@ -54,34 +54,34 @@ def random_board():
 
     return board
 
-    # TODO: still sort of broken! Works for some cases but need to verify a few
-    def dpll_sudoku(board, x_idx, y_idx, cells_filled, n):
-        # Base cases: either invalid board, or filled properly
-        if not check_still_viable(board):
-            return False
-        elif cells_filled == n**2:
-            return True
-        else:
-            # Fill with a guess if not empty
-            if board[x_idx][y_idx] == 0:
-                for i in range(1, n+1):
-                    board[x_idx][y_idx] = i
-                    if x_idx < n-1:
-                        viable = dpll_sudoku(board, x_idx+1, y_idx, cells_filled+1, n)
-                    else:
-                        viable = dpll_sudoku(board, 0, y_idx+1, cells_filled+1, n)
-                    # Check if viable path -- if so, we have a solution
-                    if viable:
-                        return True
+def dpll_sudoku(board, x_idx, y_idx, cells_filled, n):
+# Base cases: either invalid board, or filled properly
+if not check_still_viable(board):
+    return False
+elif cells_filled == n**2:
+    return True
+else:
+    # Fill with a guess if not empty
+    if board[x_idx][y_idx] == 0:
+        for i in range(1, n+1):
+            board[x_idx][y_idx] = i
+            if x_idx < n-1:
+                viable = dpll_sudoku(board, x_idx+1, y_idx, cells_filled+1, n)
             else:
-                if x_idx < n-1:
-                    viable = dpll_sudoku(board, x_idx+1, y_idx, cells_filled+1, n)
-                else:
-                    viable = dpll_sudoku(board, 0, y_idx+1, cells_filled+1, n)
-                # Check if viable path -- if so, we have a solution
-                if viable:
-                    return True
-        return False
+                viable = dpll_sudoku(board, 0, y_idx+1, cells_filled+1, n)
+            # Check if viable path -- if so, we have a solution
+            if viable:
+                return True
+        board[x_idx][y_idx] = 0
+    else:
+        if x_idx < n-1:
+            viable = dpll_sudoku(board, x_idx+1, y_idx, cells_filled+1, n)
+        else:
+            viable = dpll_sudoku(board, 0, y_idx+1, cells_filled+1, n)
+        # Check if viable path -- if so, we have a solution
+        if viable:
+            return True
+return False
 
 if __name__ == '__main__':
     # TODO: this algo mostly produces no instances I think
