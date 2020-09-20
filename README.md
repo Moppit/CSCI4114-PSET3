778f6d802022411bffde1183bcc53054b6d5eac7
Moppit <michelletranusa@gmail.com>
2020-09-20 00:54:28 -0600

    Cleaned up files and added readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # CSCI4114-PSET3
+
+NP-Complete programs are notoriously hard to solve, especially as the problem instances grow in size. However, due to years upon years of research efforts, CNF-SAT solvers have gotten incredibly good, so much so that reducing other NP-Complete problems to CNF-SAT and running them through a SAT solver has become even more efficient than optimzied algorithms specific to the chosen NP-Complete problem. 
+
+This project demonstrates exactly that: the selected problem, Sudoku for dimensions of length n, is an NP-Complete problem. There are two algorithms in this repository to solve it:
+
+1. DPLL-style backtracking algorithm
+2. Sudoku-SAT Reduction + Python-SAT's Glucose3 SAT Solver
+
+As we will see from running both algorithms, reducing Sudoku to SAT and then running it through a state-of-the-art SAT Solver is much more efficient than trying to write a Sudoku-specific algorithm (albeit, this implementation could be further optimized).
+
+## Usage
+To run this file, first ensure that Python-SAT is installed. You can install it with pip:
+```
+> pip install python-sat
+```
+Some test code already exists in the file, though due to the slow nature of the DPLL algorithm, will be quite lengthy to run. It is recommended that you alter the main code before running the code with:
+```
+> python main.py
+```
+
+## DPLL Algorithm
+A DPLL algorithm is a brute force backtracking algorithm that eliminates subsets of configurations by discarding paths as soon as a contradiction is found. For Sudoku, that means it attempts to fill in a cell with a value from 1 to n, and if the board is still valid after that guess, tries to fill in more cells. If the board is invalid, the algorithm tries a different guess before moving on.
+
+This particular DPLL algorithm has constraint propagation implemented, which makes it slightly faster than the naive algorithm.
+
+The DPLL algorithm is implemented in `dpll_sudoku()`.
+
+## Reduction to CNF-SAT
+The reduction to SAT encodes 3 main rules for Sudoku:
+
+1. Every cell must contain a single value from 1 to n
+2. Every cell must be different from other cells in the same row, column, or local block.
+3. Given cells (i.e. cells that have been filled in) must keep their given value.
+
+The Sudoku to SAT reduction algorithm is implemented in `reduction_pythonSAT()`.
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import numpy as np
 import math
-from itertools import permutations
+import time
 from pysat.solvers import Glucose3
+from itertools import permutations
+
+#------------------------------------------------------------#
+#                  General Helper Functions                  #
+#------------------------------------------------------------#
 
 def check_still_viable(board):
     # Get n val and its root
@@ -35,86 +40,168 @@ def check_still_viable(board):
 
     return True
 
-def random_board():
-    # Generate a random number as dimension n
-    n = np.random.randint(4,16) # Use this once you get past testing algo
-    # n = np.random.randint(4,10)
-    n = math.floor(math.sqrt(n))**2
-    
-    #idea for preventing immediately conflicting boards:
-    #start with empty board, 0's everywhere
-    #whatever percent p of filled squares is
-    #pick random board positions
-    #fill with random value, except check that the value is legal
-    #if not legal, either try antoher value in same cell, or just try another place altogether. idk which is more 'random'
-    
-    #n x n board filled with 0's
+def random_board(n):
+    """
+    Idea for preventing immediately conflicting boards:
+    - Start with empty board, 0's everywhere
+    - Whatever percent p of filled squares is
+    - Pick random board position
+    - Fill with random value, except check that the value is legal
+    - If not legal, either try antoher value in same cell
+    """
+    # Generate n x n board filled with 0's and calculate num cells to fill f
     board_2 = np.zeros((n,n), dtype=int)
+    f = int(0.3 * n * n)
     
-    #let's go for 25% filled
-    f = int(0.25 * n * n)
-    
-    #we want to fill f number of cells
+    # Fill f number of cells
     while (f > 0):
         f -= 1
-        
-        #random row and col, therefore a random cell
+
+        # Generate random cell position and value
         row = np.random.randint(0,n)
         col = np.random.randint(0,n)
-        #value v we want to insert into cell
         v = np.random.randint(1,n+1)
         board_2[row,col] = v
         
-        #counter to prevent infinite loop if a cell is impossible to legally fill for some reason
-        #idk if this will ever trigger, as an unfillable cell implies a broken board, but idk 
-        #better to be safe than sorry
+        # Counter to prevent infinite loop if a cell is impossible to legally fill for some reason
         count = 0
         
-        #if that value fucks up the board, try a new v
+        # If added value messes up the board, try a new v
         while (check_still_viable(board_2) == False or count > n):
             count += 1
             
-            #try v + 1, mod n+1, so only values [0,n] are possible
+            # Ensure only values [0,n] are possible (but skip 0 since that represents an empty cell)
             v = (v + 1) % (n + 1)
-            
-            #v=0 makes an empty cell, so skip that value
             if (v==0):
                 v = 1
             
-            #reattempt value
+            # Reattempt value
             board_2[row,col] = v
     
     return board_2
 
-def dpll_sudoku(board, x_idx, y_idx, cells_filled, n):
+#------------------------------------------------------------#
+#       DPLL-Style Backtracking Algo Helper Functions        #
+#------------------------------------------------------------#
+
+def constraint_propagation(board):
+    # Dictionary holds each cell and the values it's allowed to be
+    dictionary = {}
+    filled = []
+    n = len(board)
+    root = int(math.sqrt(n))
+    
+    # Start off each cell with being allowed to be all values
+    for r in range(n):
+        for c in range(len(board[r])):
+            if board[r][c] != 0:
+                dictionary[(r,c)] = [board[r][c]]
+                filled.append((r,c))
+            else:
+                dictionary[(r,c)] = [i for i in range(1,len(board)+1)]
+    
+    # Part 1: If we know a square has to be one value, eliminate that value from the squares around it
+    for coordinate in filled:
+        to_remove = dictionary[coordinate][0]
+        for i in range(len(board)):
+            check_row = dictionary[(coordinate[0], i)]
+            check_col = dictionary[(i, coordinate[1])]
+            # Go through all in same row and delete the value
+            if to_remove in check_row and (coordinate[0], i) != coordinate:
+                check_row.remove(to_remove)
+            # Go through all in same col and delete the value
+            if to_remove in check_col and (i, coordinate[1]) != coordinate:
+                check_col.remove(to_remove)
+        # Go through all in same neighbor block and delete the value
+        cell_start_r = (coordinate[0] // root)*root
+        cell_start_c = (coordinate[1] // root)*root
+        for sub_row in range(cell_start_r, cell_start_r + root):
+            for sub_col in range(cell_start_c, cell_start_c + root):
+                check_cell = dictionary[(sub_row, sub_col)]
+                if sub_row != coordinate[0] or sub_col != coordinate[1]:
+                    if to_remove in check_cell:
+                        check_cell.remove(to_remove)
+    
+    # Part 2: Check through the rest. If there is a value that doesn't belong to others, must be current
+    for coordinate in dictionary:
+        if coordinate not in filled:
+            # If not already filled, look at all neighbors and see if neighbors have same value
+            for val in dictionary[coordinate]:
+                assign = True
+                # Rows and cols
+                for i in range(len(board)):
+                    row = dictionary[(coordinate[0], i)]
+                    col = dictionary[(i, coordinate[1])]
+                    # Go through all in same row check if there
+                    if to_remove in row and (coordinate[0], i) != coordinate:
+                        assign = False
+                        break
+                    # Go through all in same col and check if there
+                    if to_remove in col and (i, coordinate[1]) != coordinate:
+                        assign = False
+                        break
+                        
+                # Sub cells
+                # Go through all in same neighbor block and check if there
+                cell_start_r = (coordinate[0] // root)*root
+                cell_start_c = (coordinate[1] // root)*root
+                for sub_row in range(cell_start_r, cell_start_r + root):
+                    for sub_col in range(cell_start_c, cell_start_c + root):
+                        check_cell = dictionary[(sub_row, sub_col)]
+                        if sub_row != coordinate[0] or sub_col != coordinate[1]:
+                            if to_remove in check_cell:
+                                assign = False
+                                break
+                
+                if assign:
+                    dictionary[coordinate] = [val]
+                    break
+    
+    return dictionary
+
+#------------------------------------------------------------#
+#               DPLL-Style Backtracking Algo                 #
+#------------------------------------------------------------#
+"""
+Function dpll_sudoku()
+@param board: 2D numpy array with integers from 0-n (0 representing empty cells)
+@param cells_filled: int representing number of cells filled thus far
+@param n: dimension of board
+@param ordered: list of tuples indicating possible values for each cell
+@return: boolean, whether the board is satisfiable or not
+"""
+def dpll_sudoku(board, cells_filled, n, ordered):
     # Base cases: either invalid board, or filled properly
     if not check_still_viable(board):
         return False
     elif cells_filled == n**2:
         return True
+    elif len(ordered) == 0:
+        return False
     else:
         # Fill with a guess if not empty
+        # ordered of form: [((0, 1), [5]), ((0, 2), [8]), ((0, 3), [2,3,4])]
+        cell_info = ordered[0]
+        x_idx = cell_info[0][0]
+        y_idx = cell_info[0][1]
         if board[x_idx][y_idx] == 0:
-            for i in range(1, n+1):
-                board[x_idx][y_idx] = i
-                if x_idx < n-1:
-                    viable = dpll_sudoku(board, x_idx+1, y_idx, cells_filled+1, n)
-                else:
-                    viable = dpll_sudoku(board, 0, y_idx+1, cells_filled+1, n)
+            # Get dictionary of possible values
+            for item in cell_info[1]:
+                board[x_idx][y_idx] = item
                 # Check if viable path -- if so, we have a solution
-                if viable:
+                if dpll_sudoku(board, cells_filled+1, n, ordered[1:]):
                     return True
+            # Reset value if check doesn't work and we backtrack
             board[x_idx][y_idx] = 0
         else:
-            if x_idx < n-1:
-                viable = dpll_sudoku(board, x_idx+1, y_idx, cells_filled+1, n)
-            else:
-                viable = dpll_sudoku(board, 0, y_idx+1, cells_filled+1, n)
-            # Check if viable path -- if so, we have a solution
-            if viable:
+            # Use given value and check if viable path -- if so, we have a solution
+            if dpll_sudoku(board, cells_filled+1, n, ordered[1:]):
                 return True
     return False
 
+#------------------------------------------------------------#
+#       Sudoku to SAT Reduction Algo Helper Functions        #
+#------------------------------------------------------------#
 # This func. converts a number to a binary string representation (0b0000 = 1)
 def to_binary(n, k):
     n = n-1
@@ -131,7 +218,8 @@ def to_binary(n, k):
 
 # This function generates a comparison block of SAT
 def gen_compare_block_python_SAT(lit1, lit2, k):
-    # Every combo of negated duos -- permutations of k literals + k negated, choose k -- but use set to filter repeats
+    # Every combo of negated bit pairs
+    # Permutations of k literals + k negated, choose k -- but use set to filter repeats
     # Generate a string of 1's and zeros to indicate which should be negated (0's are negated)
     new_clauses = []
     all_combos = permutations('0'*k + '1'*k, k)
@@ -149,10 +237,21 @@ def gen_compare_block_python_SAT(lit1, lit2, k):
         new_clauses.append(sub_clause)
     return new_clauses
 
+#------------------------------------------------------------#
+#               Sudoku to SAT Reduction Algo                 #
+#------------------------------------------------------------#
+"""
+Function reduction_pythonSAT()
+@param board: 2D numpy array with integers from 0-n (0 representing empty cells)
+@param g: Glucose3 SAT solver instance to which we will add clauses
+@return: Glucose3 SAT solver instances g
+"""
 def reduction_pythonSAT(board, g):
-    # Ensure there will never be an empty setup -- since 0 will not be used anywhere else (all clauses should hav >= 2 digits)
-    # we can guarantee that it won't affect the outcome -- you can just always set to true
+    # Ensure there will never be an empty setup -- since 1 will not be used anywhere else 
+    # (all clauses should hav >= 2 digits)
+    # We can guarantee that it won't affect the outcome -- you can just always set to true
     g.add_clause([1])
+
     # Calculate essential constants
     n = len(board)
     root = int(math.sqrt(n))
@@ -160,8 +259,9 @@ def reduction_pythonSAT(board, g):
     invalid_bin_vals = [to_binary(i, k) for i in range(n+1, 2**k+1)]
     
     # ORDERING STEPS FROM SMALLEST TO LARGEST CLAUSES (for unit propagation)
+    # Note: these steps are out of order because they are numbered according to proof in the pset
     
-    # Step 1: Encode filled in cells
+    # Step 3: Encode filled in cells
     for r in range(n):
         for c in range(len(board[r])):
             if board[r][c] != 0:
@@ -172,7 +272,7 @@ def reduction_pythonSAT(board, g):
                     else:
                         g.add_clause([-1*int(str(r+1) + str(c+1) + str(bit_idx+1))])
     
-    # Step 2: Go through every cell and make sure bit values are definitely not invalid
+    # Step 1: Go through every cell and make sure bit values are definitely not invalid
     for r in range(n):
         for c in range(len(board[r])):
             for val in invalid_bin_vals:
@@ -182,31 +282,31 @@ def reduction_pythonSAT(board, g):
                     if val[bit_idx] == '1':
                         factor = -1
                     new_clause.append(factor*int(str(r+1) + str(c+1) + str(bit_idx+1)))
-                # Write off string to file
                 g.add_clause(new_clause)
     
-    # Step 3: Ensure each cell is different from its row, column, and local block neighbors
-    # Loop through every cell in the board -- can probably combine this with steps 1 and 3
-    # (we don't actually have to know values though -- just use indices) [row and col = n, cell sqrt(n) x sqrt(n)]
+    # Step 2: Ensure each cell is different from its row, column, and local block neighbors
     for r in range(n):
         for c in range(len(board[r])):
+            # Go through row
             for row_idx in range(n):
                 if row_idx != r:
                     # Generate block of comparison code for current cell and cell to differentiate
+                    # Adding one to all values because Python-SAT only deals with non-zero values
                     new_clauses = gen_compare_block_python_SAT(str(r+1) + str(c+1), str(row_idx+1) + str(c+1), k)
-                    # Write block of comparison code to file
+                    # Add block to g
                     for clause in new_clauses:
                         g.add_clause(clause)
-                    
+
+            # Go through column
             for col_idx in range(n):
                 if col_idx != c:
                     # Generate block of comparison code for current cell and cell to differentiate
                     new_clauses = gen_compare_block_python_SAT(str(r+1) + str(c+1), str(r+1) + str(col_idx+1), k)
-                    # Write block of comparison code to file
+                    # Add block to g
                     for clause in new_clauses:
                         g.add_clause(clause)
             
-            # Get cell and do generation
+            # Get local block and do generation
             cell_start_r = (r // root)*root
             cell_start_c = (c // root)*root
             for sub_row in range(cell_start_r, cell_start_r + root):
@@ -214,34 +314,49 @@ def reduction_pythonSAT(board, g):
                     if sub_row != r and sub_col != c:
                         # Generate block of comparison code for current cell and cell to differentiate
                         new_clauses = gen_compare_block_python_SAT(str(r+1) + str(c+1), str(sub_row+1) + str(sub_col+1), k)
-                        # Write block of comparison code to file
+                        # Add block to g
                         for clause in new_clauses:
                             g.add_clause(clause)
     return g
 
-
+#------------------------------------------------------------#
+#                       Testing Code                         #
+#------------------------------------------------------------#
 if __name__ == '__main__':
-    countTrue = 0
-    total = 10
-    dictionary = {}
-    for i in range(total):
-        test_pythonSAT = random_board()
-        g = Glucose3()
-        new_g = reduction_pythonSAT(test_pythonSAT, g)
-        
-        length = len(test_pythonSAT)
-        if length not in dictionary:
-            dictionary[length] = 1
-        else:
-            dictionary[length] += 1
-        
-        truth = new_g.solve()
-        if truth:
-            countTrue += 1
-            
-        print(length, truth)
+    # Only do up to 16x16, as DPLL algorithm can barely handle 9 already
+    total = 100
+    sizes = [4,9,16]
 
-    print(countTrue, 'boards were true out of', total)
-    print(dictionary)
+    # For each size, indicate
+    for size in sizes:
+        print('Working on size:', size)
+        for i in range(total):
+            print('Iteration', i)
+            board = random_board(size)
+
+            # Time DPLL solution
+            start = time.time()
+            dictionary = constraint_propagation(board)
+            ordered = sorted(dictionary.items(), key = lambda kv:(len(kv[1]), kv[0])) # Sorts by most constrained
+            dpll_sudoku(board, 0, len(board), ordered)
+            end = time.time()
+
+            # Time is written in seconds
+            file_write = open('dpll' + str(size) + '.csv', 'a')
+            file_write.write(str(size) + ',' + str(i) + ',' + str(end-start) + '\n')
+            file_write.close()
+
+            # Time reduction solution
+            start = time.time()
+            new_g = reduction_pythonSAT(test_pythonSAT, g)
+            truth = new_g.solve()
+            end = time.time()
+
+            # Time is written in seconds
+            file_write_2 = open('sat_red' + str(size) + '.csv', 'a')
+            file_write_2.write(str(size) + ',' + str(i) + ',' + str(end-start) + '\n')
+            file_write_2.close()
+            
+        print('Done with size', size)
 
 
